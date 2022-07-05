08bbfa02115369b2b03ef8ac6f062486facb80c4
MattB120 <105670086+MattB120@users.noreply.github.com>
2022-07-05 21:24:48 +0000

    Initial project setup

---
+++ src/main/java/com/revature/trailmates/TrailMatesDriver.java
@@ -0,0 +1,12 @@
+package com.revature.trailmates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrailMatesDriver {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrailMatesDriver.class, args);
+    }
+}
