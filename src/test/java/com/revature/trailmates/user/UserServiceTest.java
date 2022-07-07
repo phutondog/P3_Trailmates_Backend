ff66902c5521479454e496903a03aa4fb0952c58
hislittlecuzin <hislittlecuzingames@gmail.com>
2022-07-07 14:10:32 -0400

    Edit user test cases working.

--- src/test/java/com/revature/trailmates/user/UserServiceTest.java
+++ src/test/java/com/revature/trailmates/user/UserServiceTest.java
@@ -6,16 +6,18 @@ import com.revature.trailmates.auth.dtos.requests.NewUserRequest;
 import com.revature.trailmates.user.dtos.requests.EditUserRequest;
 import com.revature.trailmates.util.custom_exception.InvalidRequestException;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class UserServiceTest {
 
     @Mock
@@ -35,58 +37,68 @@ class UserServiceTest {
     @Test
     void updateUser() {
         editUserRequest = new EditUserRequest();
-
-        editUserRequest.setId("1");
-        editUserRequest.setUsername("foo");
-        editUserRequest.setPassword("123");
-        editUserRequest.setEmail("email@email.net");
-        editUserRequest.setAge(12);
-        editUserRequest.setBio("I'm the guy");
-        editUserRequest.setRole("DEFAULT");
-
-        try {
-            //Setup:
-            when( userService.UpdateUser( anyString(), any(EditUserRequest.class) ) )
-                    .thenThrow(InvalidRequestException.class);
-            //Method
-            userService.UpdateUser("0", editUserRequest);
-            System.out.println("Succeeded?!");
-        }catch(Exception e){
-            System.out.println("Exception occured " + e.getClass());
-            assertFalse(e instanceof InvalidRequestException);
-        }
-
-
-        /*
-        loginRequest.setUsername("test");
-        loginRequest.setPassword("test");
-
-        //Mockito.verify(loginRequest).setUsername("test");
+        editUserRequest.setId("0");
 
         User dummy = new User();
-        dummy.setUsername("test");
+        dummy.setId("0");
 
-        when(userRepository.getUserByUsername(loginRequest.getUsername())).thenReturn(dummy);
-        assertEquals(dummy, authService.login(loginRequest));*/
+        Mockito.when(userRepository.getUserByID(editUserRequest.getId())).thenReturn(dummy);
+
+        //region email invalid
+        {
+            editUserRequest.setId("0");
+            editUserRequest.setUsername("foo");
+            editUserRequest.setPassword("123");
+            editUserRequest.setEmail("email");
+            editUserRequest.setAge(20);
+            editUserRequest.setBio("I'm the guy");
+            editUserRequest.setRole("DEFAULT");
+
+            Exception exception = assertThrows(RuntimeException.class, () -> userService.UpdateUser("0", editUserRequest));
+            String expectedMessage = "Email invalid!";
+            String actualMessage = exception.getMessage();
+
+            assertTrue(actualMessage.contains(expectedMessage));
+        }
+
+        //endregion
+
+        //region underage
+        {
+            editUserRequest.setId("0");
+            editUserRequest.setUsername("foo");
+            editUserRequest.setPassword("123");
+            editUserRequest.setEmail("email@email.net");
+            editUserRequest.setAge(5);
+            editUserRequest.setBio("I'm the guy");
+            editUserRequest.setRole("DEFAULT");
+
+            Exception exception = assertThrows(RuntimeException.class, () -> userService.UpdateUser("0", editUserRequest));
+            String expectedMessage = "Users must be older than 13 to use our services for Child Protection.";
+            String actualMessage = exception.getMessage();
+
+            assertTrue(actualMessage.contains(expectedMessage));
+        }
+        //endregion
+
+        //region too long bio
+        {
+            editUserRequest.setId("0");
+            editUserRequest.setUsername("foo");
+            editUserRequest.setPassword("123");
+            editUserRequest.setEmail("email@email.net");
+            editUserRequest.setAge(20);
+            editUserRequest.setBio("0123456789 0123456789 0123456789 0123456789 0123456789  0123456789 0123456789 0123456789 0123456789 0123456789 0123456789 0123456789 0123456789 0123456789 0123456789 0123456789 0123456789 0123456789 0123456789 0123456789 0123456789 0123456789 0123456789 0123456789 0123456789 0123456789 0123456789 0123456789 0123456789 0123456789 0123456789 0123456789 0123456789 0123456789 0123456789");
+            editUserRequest.setRole("DEFAULT");
+
+            Exception exception = assertThrows(RuntimeException.class, () -> userService.UpdateUser("0", editUserRequest));
+            String expectedMessage = "Bio must be less than 255 characters!";
+            String actualMessage = exception.getMessage();
+
+            assertTrue(actualMessage.contains(expectedMessage));
+        }
+        //endregion
+
     }
-/*
-
-
-    @Test
-    void register() {
-        newUserRequest.setUsername("test");
-
-        Exception exception = assertThrows(RuntimeException.class, () -> authService.register(newUserRequest));
-        String expectedMessage = " is null";
-        String actualMessage = exception.getMessage();
-
-        assertTrue(actualMessage.contains(expectedMessage));
-    }
-
-    @Test
-    void isValidEmail() {
-
-    }
- */
 
 }
